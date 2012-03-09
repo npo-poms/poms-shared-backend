3f6a666171ac3981ab4a70c8125a31a544502fba
Rico Jansen <r.jansen@vpro.nl>
2012-03-09 14:48:00 +0000

    MGNL-4533 initial
    svn path=/nl/vpro/api/trunk/; revision=42275

---
+++ api-services/src/main/java/nl/vpro/api/media/MediaService.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2012 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.api.media;
+
+/**
+ * User: rico
+ * Date: 08/03/2012
+ */
+public interface MediaService {
+
+    public String get(String urn);
+}
---
+++ api-services/src/main/java/nl/vpro/api/media/MediaServiceImpl.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2012 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.api.media;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * User: rico
+ * Date: 08/03/2012
+ */
+
+@Service("mediaService")
+public class MediaServiceImpl implements MediaService {
+
+    @Value("mediaservice.api.url")
+    String apiUrl;
+
+    public String get(String urn) {
+
+        return urn;
+    }
+
+}
