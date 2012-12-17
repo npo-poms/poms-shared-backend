61b5be1719d1ecfc8da864af1ce9c968aeb99be7
Rico Jansen <r.jansen@vpro.nl>
2012-12-17 10:52:55 +0000

    MGNL-7756 escaping illegal characters.
    svn path=/nl/vpro/api/trunk/; revision=52411

--- api-services/src/main/java/nl/vpro/api/service/search/es/MultiQueryStringConstraintBuilder.java
+++ api-services/src/main/java/nl/vpro/api/service/search/es/MultiQueryStringConstraintBuilder.java
@@ -4,6 +4,7 @@
  */
 package nl.vpro.api.service.search.es;
 
+import org.apache.lucene.queryParser.QueryParser;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryStringQueryBuilder;
 
@@ -17,6 +18,12 @@ public class MultiQueryStringConstraintBuilder extends BoolQueryBuilder {
 
     public MultiQueryStringConstraintBuilder(List<String> constraints) {
         for (String constraint : constraints) {
+            int colon = constraint.indexOf(':');
+            if (colon > -1) {
+                String field = constraint.substring(0, colon + 1);
+                String constr = constraint.substring(colon + 1);
+                constraint = field + QueryParser.escape(constr);
+            }
             must(new QueryStringQueryBuilder(constraint));
         }
     }
