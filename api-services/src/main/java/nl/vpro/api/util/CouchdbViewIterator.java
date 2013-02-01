a8d533ad858a0452ebd8fd1c1a6e0971d3f90453
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2013-02-01 13:16:33 +0000

    javadoc.
    svn path=/nl/vpro/api/trunk/; revision=53690

--- api-services/src/main/java/nl/vpro/api/util/CouchdbViewIterator.java
+++ api-services/src/main/java/nl/vpro/api/util/CouchdbViewIterator.java
@@ -1,26 +1,22 @@
 package nl.vpro.api.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.NoSuchElementException;
-
+import com.google.common.collect.UnmodifiableIterator;
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonToken;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.UnmodifiableIterator;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
 
 /**
+ * An iterator over a couchdb view that is streaming, which doesn't use any memory, so you can use for huge results.
  * @author Michiel Meeuwissen
  * @since 1.3
  */
 public class CouchdbViewIterator extends UnmodifiableIterator<JsonNode> {
-
-    private static final Logger LOG = LoggerFactory.getLogger(CouchdbViewIterator.class);
 
     private final JsonParser parser;
 
