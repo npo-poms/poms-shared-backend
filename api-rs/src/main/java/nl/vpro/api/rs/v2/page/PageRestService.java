014324cda0b150da88d1a5b6d207903789add5a6
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2013-05-16 19:23:19 +0000

    indented with spaces..
    svn path=/nl/vpro/api/trunk/; revision=56414

--- api-rs/src/main/java/nl/vpro/api/rs/v2/page/PageRestService.java
+++ api-rs/src/main/java/nl/vpro/api/rs/v2/page/PageRestService.java
@@ -21,33 +21,33 @@ import javax.ws.rs.core.MediaType;
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 public interface PageRestService {
 
-	@GET
-	@Path("/")
-	PagedResult<Page> list(
-			@QueryParam("profile") String profile,
-			@QueryParam("offset") @DefaultValue("0") Integer offset,
-			@QueryParam("limit") @DefaultValue("50") Integer limit,
-			@QueryParam("mock") @DefaultValue("false") boolean mock);
+    @GET
+    @Path("/")
+    PagedResult<Page> list(
+            @QueryParam("profile") String profile,
+            @QueryParam("offset") @DefaultValue("0") Integer offset,
+            @QueryParam("limit") @DefaultValue("50") Integer limit,
+            @QueryParam("mock") @DefaultValue("false") boolean mock);
 
-	@POST
-	@Path("/")
-	PagedResult<Page> search(
-			PageForm form,
-			@QueryParam("profile") String profile,
-			@QueryParam("offset") @DefaultValue("0") Integer offset,
-			@QueryParam("limit") @DefaultValue("50") Integer limit,
-			@QueryParam("mock") @DefaultValue("false") boolean mock);
+    @POST
+    @Path("/")
+    PagedResult<Page> search(
+            PageForm form,
+            @QueryParam("profile") String profile,
+            @QueryParam("offset") @DefaultValue("0") Integer offset,
+            @QueryParam("limit") @DefaultValue("50") Integer limit,
+            @QueryParam("mock") @DefaultValue("false") boolean mock);
 
-	/**
-	 * Retrieve a media resource, either a Program, Group or Segment, by it's id.
-	 *
-	 * @param id   existing urn or mid
-	 * @param mock whether to return a mock object
-	 * @return full Program, Group or Segment
-	 */
-	@GET
-	@Path("/{id}")
-	Page get(@PathParam("id") String id, @QueryParam("mock") @DefaultValue("false") boolean mock);
+    /**
+     * Retrieve a media resource, either a Program, Group or Segment, by it's id.
+     *
+     * @param id   existing urn or mid
+     * @param mock whether to return a mock object
+     * @return full Program, Group or Segment
+     */
+    @GET
+    @Path("/{id}")
+    Page get(@PathParam("id") String id, @QueryParam("mock") @DefaultValue("false") boolean mock);
 
 
 
