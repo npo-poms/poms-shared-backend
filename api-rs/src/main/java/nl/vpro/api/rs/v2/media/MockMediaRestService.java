1ce69c58a31b2b322cc7cd8ae9beeddb1fcbc48d
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2013-05-16 17:00:02 +0000

    API-68 API-74 API-75
    svn path=/nl/vpro/api/trunk/; revision=56411

--- api-rs/src/main/java/nl/vpro/api/rs/v2/media/MockMediaRestService.java
+++ api-rs/src/main/java/nl/vpro/api/rs/v2/media/MockMediaRestService.java
@@ -1,34 +1,39 @@
 package nl.vpro.api.rs.v2.media;
 
 import nl.vpro.domain.api.PagedResult;
-import nl.vpro.domain.media.*;
+import nl.vpro.domain.media.MediaObject;
+import nl.vpro.domain.media.MediaTestDataBuilder;
+import nl.vpro.domain.media.Program;
+import nl.vpro.domain.media.Segment;
 import nl.vpro.domain.media.search.MediaForm;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Michiel Meeuwissen
- * @since ...
+ * @since 2.0
  */
-public class MockMediaRestService implements MediaRestService {
+class MockMediaRestService implements MediaRestService {
+	private static int listSizes = 100;
 	@Override
 	public PagedResult<MediaObject> list(String profile, Integer offset, Integer limit,  boolean mock) {
-		return new PagedResult<MediaObject>(
-				Arrays.asList(
-						MediaBuilder.program().build(),
-						MediaBuilder.program().build(),
-						MediaBuilder.program().build()
-				), offset, 100);
+		return new PagedResult<>(mockList(listSizes, offset, limit), offset, listSizes);
 	}
 
 	@Override
 	public PagedResult<MediaObject> search(MediaForm form, String profile, Integer offset, Integer limit, boolean mock) {
-		return new PagedResult<MediaObject>(
-				Arrays.asList(
-						MediaBuilder.program().build(),
-						MediaBuilder.program().build(),
-						MediaBuilder.program().build()
-				), offset, 100);
+		return new PagedResult<>(mockList(listSizes, offset, limit), offset, listSizes);
+
+	}
+
+	protected List<MediaObject> mockList(int total, int offset, int limit) {
+		int numberOfResults = Math.min(total - offset, limit);
+		List<MediaObject> result = new ArrayList<>();
+		for (int i = 0; i < numberOfResults; i++) {
+			result.add(MediaTestDataBuilder.program().build());
+		}
+		return result;
 	}
 
 	@Override
