448e6863cf53375c33793f9e67e82a9eff8d3d0b
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2013-05-16 20:27:51 +0000

    indented with spaces..
    svn path=/nl/vpro/api/trunk/; revision=56420

--- api-rs/src/test/java/nl/vpro/api/rs/v2/page/PageRestServiceImplTest.java
+++ api-rs/src/test/java/nl/vpro/api/rs/v2/page/PageRestServiceImplTest.java
@@ -21,58 +21,58 @@ import static org.junit.Assert.assertEquals;
  * @since 2.0
  */
 public class PageRestServiceImplTest {
-	public static final MediaType JSON;
+    public static final MediaType JSON;
 
-	static {
-		Map<String, String> params = new HashMap<>();
-		JSON = new MediaType("application", "json", params);
-	}
+    static {
+        Map<String, String> params = new HashMap<>();
+        JSON = new MediaType("application", "json", params);
+    }
 
-	private static final ObjectMapper mapper = new ObjectMapper();
-	Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+    private static final ObjectMapper mapper = new ObjectMapper();
+    Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
 
-	@Before
-	public void setUp() throws Exception {
-		PageRestService testObject = new PageRestServiceImpl();
-		dispatcher.getRegistry().addSingletonResource(testObject);
-		XMLUnit.setIgnoreWhitespace(true);
-		XMLUnit.setIgnoreAttributeOrder(true);
+    @Before
+    public void setUp() throws Exception {
+        PageRestService testObject = new PageRestServiceImpl();
+        dispatcher.getRegistry().addSingletonResource(testObject);
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setIgnoreAttributeOrder(true);
 
-	}
+    }
 
-	@Test
-	public void testList() throws Exception {
+    @Test
+    public void testList() throws Exception {
 
-	}
+    }
 
-	@Test
-	public void testSearch() throws Exception {
+    @Test
+    public void testSearch() throws Exception {
 
-	}
+    }
 
-	@Test
-	public void testGet() throws Exception {
-		MockHttpRequest request = MockHttpRequest.get("/pages/123?mock=true");
-		MockHttpResponse response = new MockHttpResponse();
-		dispatcher.invoke(request, response);
-		assertEquals(response.getErrorMessage(), 200, response.getStatus());
-		assertEquals(JSON, response.getOutputHeaders().get("Content-Type").get(0));
-		Page page = mapper.readValue(response.getContentAsString(), Page.class);
-		assertEquals("{\"id\":\"4b748d32-8006-4f0a-8aac-6d8d5c89a847\",\"title\":\"Groot brein in klein dier\",\"body\":null,\"summary\":\"Een klein, harig beestje met het gewicht van een paperclip was mogelijk de directe voorouder van alle hedendaagse zoogdieren, waaronder de mens. Levend in de schaduw van de dinosaurussen kroop het diertje 195 miljoen jaar geleden tussen de planten door, op zoek naar insecten die het met zijn vlijmscherpe tandjes vermaalde. Het is de oudste zoogdierachtige die tot nu toe is gevonden.\",\"deepLink\":null,\"pageType\":null,\"brand\":null,\"author\":\"superuser\",\"mainImage\":null}", response.getContentAsString());
-		assertEquals("4b748d32-8006-4f0a-8aac-6d8d5c89a847", page.getId());
+    @Test
+    public void testGet() throws Exception {
+        MockHttpRequest request = MockHttpRequest.get("/pages/123?mock=true");
+        MockHttpResponse response = new MockHttpResponse();
+        dispatcher.invoke(request, response);
+        assertEquals(response.getErrorMessage(), 200, response.getStatus());
+        assertEquals(JSON, response.getOutputHeaders().get("Content-Type").get(0));
+        Page page = mapper.readValue(response.getContentAsString(), Page.class);
+        assertEquals("{\"id\":\"4b748d32-8006-4f0a-8aac-6d8d5c89a847\",\"title\":\"Groot brein in klein dier\",\"body\":null,\"summary\":\"Een klein, harig beestje met het gewicht van een paperclip was mogelijk de directe voorouder van alle hedendaagse zoogdieren, waaronder de mens. Levend in de schaduw van de dinosaurussen kroop het diertje 195 miljoen jaar geleden tussen de planten door, op zoek naar insecten die het met zijn vlijmscherpe tandjes vermaalde. Het is de oudste zoogdierachtige die tot nu toe is gevonden.\",\"deepLink\":null,\"pageType\":null,\"brand\":null,\"author\":\"superuser\",\"mainImage\":null}", response.getContentAsString());
+        assertEquals("4b748d32-8006-4f0a-8aac-6d8d5c89a847", page.getId());
 
-	}
+    }
 
-	@Test
-	public void testGetXml() throws Exception {
-		MockHttpRequest request = MockHttpRequest.get("/pages/123.xml?mock=true");
-		MockHttpResponse response = new MockHttpResponse();
-		dispatcher.invoke(request, response);
-		assertEquals(response.getErrorMessage(), 200, response.getStatus());
-		assertEquals(JSON, response.getOutputHeaders().get("Content-Type").get(0));
-		Page page = mapper.readValue(response.getContentAsString(), Page.class);
-		assertEquals("{\"id\":\"4b748d32-8006-4f0a-8aac-6d8d5c89a847\",\"title\":\"Groot brein in klein dier\",\"body\":null,\"summary\":\"Een klein, harig beestje met het gewicht van een paperclip was mogelijk de directe voorouder van alle hedendaagse zoogdieren, waaronder de mens. Levend in de schaduw van de dinosaurussen kroop het diertje 195 miljoen jaar geleden tussen de planten door, op zoek naar insecten die het met zijn vlijmscherpe tandjes vermaalde. Het is de oudste zoogdierachtige die tot nu toe is gevonden.\",\"deepLink\":null,\"pageType\":null,\"brand\":null,\"author\":\"superuser\",\"mainImage\":null}", response.getContentAsString());
-		assertEquals("4b748d32-8006-4f0a-8aac-6d8d5c89a847", page.getId());
+    @Test
+    public void testGetXml() throws Exception {
+        MockHttpRequest request = MockHttpRequest.get("/pages/123.xml?mock=true");
+        MockHttpResponse response = new MockHttpResponse();
+        dispatcher.invoke(request, response);
+        assertEquals(response.getErrorMessage(), 200, response.getStatus());
+        assertEquals(JSON, response.getOutputHeaders().get("Content-Type").get(0));
+        Page page = mapper.readValue(response.getContentAsString(), Page.class);
+        assertEquals("{\"id\":\"4b748d32-8006-4f0a-8aac-6d8d5c89a847\",\"title\":\"Groot brein in klein dier\",\"body\":null,\"summary\":\"Een klein, harig beestje met het gewicht van een paperclip was mogelijk de directe voorouder van alle hedendaagse zoogdieren, waaronder de mens. Levend in de schaduw van de dinosaurussen kroop het diertje 195 miljoen jaar geleden tussen de planten door, op zoek naar insecten die het met zijn vlijmscherpe tandjes vermaalde. Het is de oudste zoogdierachtige die tot nu toe is gevonden.\",\"deepLink\":null,\"pageType\":null,\"brand\":null,\"author\":\"superuser\",\"mainImage\":null}", response.getContentAsString());
+        assertEquals("4b748d32-8006-4f0a-8aac-6d8d5c89a847", page.getId());
 
-	}
+    }
 }
