6e1ecfd5b9a357735e0d996a37952ee679c0d74b
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2013-05-27 17:26:27 +0000

    test for CorsPolicyImpl
    svn path=/nl/vpro/api/trunk/; revision=56753

---
+++ api-rs/src/test/java/nl/vpro/api/cors/CorsPolicyImplTest.java
@@ -0,0 +1,26 @@
+package nl.vpro.api.cors;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 2.0
+ */
+public class CorsPolicyImplTest {
+
+
+    @Test
+    public void test() throws Exception {
+        CorsPolicyImpl impl = new CorsPolicyImpl(true, "classpath:/cors/policy.properties");
+
+        assertTrue(impl.allowedOriginAndMethod("localhost", "GET"));
+        assertTrue(impl.isEnabled());
+
+        assertFalse(impl.allowedOriginAndMethod("localhost", "PUT"));
+    }
+
+
+}
