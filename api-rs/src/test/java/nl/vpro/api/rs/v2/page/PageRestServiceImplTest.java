535f6b9982c78268ff119e9d22b2a7baf68d140f
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2013-06-05 09:31:57 +0000

    failing test cases
    svn path=/nl/vpro/api/trunk/; revision=56925

--- api-rs/src/test/java/nl/vpro/api/rs/v2/page/PageRestServiceImplTest.java
+++ api-rs/src/test/java/nl/vpro/api/rs/v2/page/PageRestServiceImplTest.java
@@ -1,11 +1,11 @@
 package nl.vpro.api.rs.v2.page;
 
-import nl.vpro.api.rs.v2.AbstractRestServiceImplTest;
-import nl.vpro.domain.api.Result;
-import nl.vpro.domain.api.SearchResult;
-import nl.vpro.domain.api.page.PageForm;
-import nl.vpro.domain.api.page.PageService;
-import nl.vpro.domain.page.Page;
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXB;
+
 import org.codehaus.jackson.type.TypeReference;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
@@ -13,10 +13,12 @@ import org.jboss.resteasy.mock.MockHttpRequest;
 import org.jboss.resteasy.mock.MockHttpResponse;
 import org.junit.Test;
 
-import javax.ws.rs.core.MediaType;
-import javax.xml.bind.JAXB;
-import java.io.ByteArrayOutputStream;
-import java.io.StringReader;
+import nl.vpro.api.rs.v2.AbstractRestServiceImplTest;
+import nl.vpro.domain.api.Result;
+import nl.vpro.domain.api.SearchResult;
+import nl.vpro.domain.api.page.PageForm;
+import nl.vpro.domain.api.page.PageService;
+import nl.vpro.domain.page.Page;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -140,7 +142,7 @@ public class PageRestServiceImplTest extends AbstractRestServiceImplTest {
 
         Page page = mapper.readValue(response.getContentAsString(), Page.class);
 
-        assertEquals("{\"title\":\"Groot brein in klein dier\",\"body\":\"bla bla bla bla\",\"summary\":\"Een klein, harig beestje met het gewicht van een paperclip was mogelijk de directe voorouder van alle hedendaagse zoogdieren, waaronder de mens. Levend in de schaduw van de dinosaurussen kroop het diertje 195 miljoen jaar geleden tussen de planten door, op zoek naar insecten die het met zijn vlijmscherpe tandjes vermaalde. Het is de oudste zoogdierachtige die tot nu toe is gevonden.\",\"deepLink\":\"http://www.wetenschap24.nl/nieuws/artikelen/2001/mei/Groot-brein-in-klein-dier.html\",\"pageType\":\"Artikel\",\"brand\":{\"site\":\"http://www.wetenschap24.nl\",\"title\":\"Wetenschap 24\"},\"author\":\"superuser\",\"mainImage\":{\"url\":\"http://www.wetenschap24.nl/.imaging/stk/wetenschap/vtk-imagegallery-normal/media/wetenschap/noorderlicht/artikelen/2001/May/3663525/original/3663525.jpeg\"},\"mediaIds\":[\"urn:vpro:media:program:1234\",\"urn:vpro:media:group:4321\"],\"id\":\"4b748d32-8006-4f0a-8aac-6d8d5c89a847\"}", response.getContentAsString());
+        assertEquals("{\"title\":\"Groot brein in klein dier\",\"body\":\"bla bla bla bla\",\"summary\":\"Een klein, harig beestje met het gewicht van een paperclip was mogelijk de directe voorouder van alle hedendaagse zoogdieren, waaronder de mens. Levend in de schaduw van de dinosaurussen kroop het diertje 195 miljoen jaar geleden tussen de planten door, op zoek naar insecten die het met zijn vlijmscherpe tandjes vermaalde. Het is de oudste zoogdierachtige die tot nu toe is gevonden.\",\"deepLink\":\"http://www.wetenschap24.nl/nieuws/artikelen/2001/mei/Groot-brein-in-klein-dier.html\",\"pageType\":\"Artikel\",\"brand\":{\"site\":\"http://www.wetenschap24.nl\",\"title\":\"Wetenschap 24\"},\"author\":\"superuser\",\"mainImage\":{\"url\":\"http://www.wetenschap24.nl/.imaging/stk/wetenschap/vtk-imagegallery-normal/media/wetenschap/noorderlicht/artikelen/2001/May/3663525/original/3663525.jpeg\"},\"mediaIds\":[\"urn:vpro:media:program:1234\",\"urn:vpro:media:group:4321\"],\"sortDate\":1370424584330,\"id\":\"4b748d32-8006-4f0a-8aac-6d8d5c89a847\"}", response.getContentAsString());
 
         assertEquals("4b748d32-8006-4f0a-8aac-6d8d5c89a847", page.getId());
         assertEquals("http://www.wetenschap24.nl/.imaging/stk/wetenschap/vtk-imagegallery-normal/media/wetenschap/noorderlicht/artikelen/2001/May/3663525/original/3663525.jpeg",
@@ -181,6 +183,7 @@ public class PageRestServiceImplTest extends AbstractRestServiceImplTest {
                 "    <pages:id>urn:vpro:media:program:1234</pages:id>\n" +
                 "    <pages:id>urn:vpro:media:group:4321</pages:id>\n" +
                 "  </pages:mediaIds>\n" +
+                "  <pages:sortDate>2013-06-05T11:29:44.330+02:00</pages:sortDate>\n" +
                 "</pages:page>\n";
 
         Diff diff = XMLUnit.compareXML(expected, response.getContentAsString());
