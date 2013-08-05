4f6052c9ae4c55d891b9b8d497e4cbc51d242312
Roelof Jan Koekoek <r.koekoek@vpro.nl>
2013-08-05 17:42:20 +0000

    PLP-13 Dependency clean-up
    svn path=/nl/vpro/api/trunk/; revision=58359

--- api-rs/src/main/java/nl/vpro/api/rs/v2/media/SuggestRestService.java
+++ api-rs/src/main/java/nl/vpro/api/rs/v2/media/SuggestRestService.java
@@ -4,23 +4,17 @@
  */
 package nl.vpro.api.rs.v2.media;
 
-import nl.vpro.domain.api.Result;
-import nl.vpro.domain.api.Suggestion;
-import org.codehaus.enunciate.jaxrs.ResponseCode;
-import org.codehaus.enunciate.jaxrs.StatusCodes;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+import nl.vpro.domain.api.Result;
+import nl.vpro.domain.api.Suggestion;
 
 /**
  * @author Roelof Jan Koekoek
  * @since 2.0
  */
 @Path("/suggest")
-@StatusCodes({
-    @ResponseCode(code = 200, condition = "success"),
-    @ResponseCode(code = 400, condition = "bad request"),
-    @ResponseCode(code = 500, condition = "server error")})
 @Produces({MediaType.APPLICATION_JSON + "; charset=UTF-8", MediaType.APPLICATION_XML + "; charset=UTF-8"})
 public interface SuggestRestService {
 
