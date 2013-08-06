c6175ef49fd8aeaee8f5544b6e53d3cb13f0231f
Roelof Jan Koekoek <r.koekoek@vpro.nl>
2013-08-06 07:19:58 +0000

    PLP-13 Some docs
    svn path=/nl/vpro/api/trunk/; revision=58366

--- api-rs/src/main/java/nl/vpro/swagger/SwaggerApplication.java
+++ api-rs/src/main/java/nl/vpro/swagger/SwaggerApplication.java
@@ -19,6 +19,9 @@ import nl.vpro.api.rs.v2.media.MediaRestService;
 import nl.vpro.api.rs.v2.page.PageRestService;
 
 /**
+ * Static helper to expose a dummy JAX-RS Application. Swagger needs such an application to expose the API docs for
+ * the given endpoints.
+ *
  * @author Roelof Jan Koekoek
  * @since 2.0
  */
@@ -43,6 +46,13 @@ public class SwaggerApplication extends Application {
         return singletons;
     }
 
+    /**
+     * Inject Swagger annotated JAX-RS api's you want to publish. Swagger only calls #getSingletons on the first
+     * API documentation request, so you must take care that your services get injected before an application accepts
+     * web requests. (Using @PostConstruct on your services is a viable solution)
+     *
+     * @param services
+     */
     public static void inject(Object... services) {
         singletons.addAll(Arrays.asList(services));
     }
