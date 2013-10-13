8e7508f45067b7d54ae61c56edf5c2ec2126544b
Roelof Jan Koekoek <r.koekoek@vpro.nl>
2013-10-13 19:12:31 +0000

    VR-21 Event based real time media high scores
    svn path=/nl/vpro/api/trunk/; revision=59988

---
+++ api-rs/src/main/java/nl/vpro/api/rs/v2/statistics/StatsRestService.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.api.rs.v2.statistics;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import nl.vpro.domain.api.Constants;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 2.2
+ */
+@Path(StatsRestService.PATH)
+@Produces({MediaType.APPLICATION_JSON + "; charset=utf-8", MediaType.APPLICATION_XML + "; charset=utf-8"})
+public interface StatsRestService {
+    public static final String PATH = "/stats";
+
+    @GET
+    @Path("/media")
+    Response stats(
+        @QueryParam("period") @DefaultValue("hour") String period,
+        @QueryParam("properties") String properties,
+        @QueryParam("offset") @DefaultValue("0") Long offset,
+        @QueryParam("max") @DefaultValue(Constants.MAX_RESULTS_STRING) Integer max
+    );
+
+    @GET
+    @Path("/media/broadcasters/{broadcaster}")
+    Response statsPerBroadcaster(
+        @PathParam("broadcaster") String broadcaster,
+        @QueryParam("period") @DefaultValue("hour") String period,
+        @QueryParam("properties") String properties,
+        @QueryParam("offset") @DefaultValue("0") Long offset,
+        @QueryParam("max") @DefaultValue(Constants.MAX_RESULTS_STRING) Integer max
+    );
+
+    @GET
+    @Path("/media/portals/{portal}")
+    Response statsPerPortal(
+        @PathParam("portal") String portal,
+        @QueryParam("period") @DefaultValue("hour") String period,
+        @QueryParam("properties") String properties,
+        @QueryParam("offset") @DefaultValue("0") Long offset,
+        @QueryParam("max") @DefaultValue(Constants.MAX_RESULTS_STRING) Integer max
+    );
+
+}
---
+++ api-rs/src/main/java/nl/vpro/api/rs/v2/statistics/StatsRestServiceImpl.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.api.rs.v2.statistics;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.wordnik.swagger.annotations.*;
+
+import nl.vpro.api.rs.v2.Responses;
+import nl.vpro.domain.api.Constants;
+import nl.vpro.domain.api.MediaResult;
+import nl.vpro.domain.api.transfer.MediaTransferResult;
+import nl.vpro.statistic.StatsService;
+import nl.vpro.statistic.media.PlayService;
+import nl.vpro.swagger.SwaggerApplication;
+import nl.vpro.transfer.media.PropertySelection;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 2.1
+ */
+@Service
+@Api(value = StatsRestService.PATH, description = "The media API")
+public class StatsRestServiceImpl implements StatsRestService {
+
+    @Value("${api.stats.expose}")
+    private boolean expose;
+
+    private final PlayService playService;
+
+    @Autowired
+    public StatsRestServiceImpl(PlayService playService) {
+        this.playService = playService;
+    }
+
+    @PostConstruct
+    private void init() {
+        if(expose) {
+            SwaggerApplication.inject(this);
+        }
+    }
+
+    @GET
+    @Path("/media")
+    @ApiOperation(httpMethod = "get",
+        value = "Top media overall",
+        notes = "High score list containing the most played media")
+    @ApiErrors(value = {@ApiError(code = 400, reason = "Bad request"), @ApiError(code = 500, reason = "Server error")})
+    @Override
+    public Response stats(
+        @ApiParam(value = "On of hour/day/week") @QueryParam("period") @DefaultValue("hour") String periodString,
+        @ApiParam(value = "Optimise media result for these returned properties <a href=\"#!/media/load_get_0\">See Media API</a>", required = false) @QueryParam("properties") String properties,
+        @ApiParam @QueryParam("offset") @DefaultValue("0") Long offset,
+        @ApiParam @QueryParam("max") @DefaultValue(Constants.MAX_RESULTS_STRING) Integer max
+    ) {
+        StatsService.Period period;
+        try {
+            period = StatsService.Period.valueOf(periodString);
+        } catch(IllegalArgumentException e) {
+            return Responses.clientError("Can't parse period '{}'");
+        }
+
+        MediaResult result = playService.mediaScoreOverall(period, offset, max);
+        return filteredMediaResult(result, properties);
+    }
+
+    @GET
+    @Path("/media/broadcasters/{broadcaster}")
+    @ApiOperation(httpMethod = "get",
+        value = "Top media per broadcaster",
+        notes = "High score list containing the most played media per broadcaster")
+    @ApiErrors(value = {@ApiError(code = 400, reason = "Bad request"), @ApiError(code = 500, reason = "Server error")})
+    @Override
+    public Response statsPerBroadcaster(
+        @ApiParam(required = true, defaultValue = "VPRO") @PathParam("broadcaster") String broadcaster,
+        @ApiParam(value = "On of hour/day/week") @QueryParam("period") @DefaultValue("hour") String periodString,
+        @ApiParam(value = "Optimise media result for these returned properties <a href=\"#!/media/load_get_0\">See Media API</a>", required = false) @QueryParam("properties") String properties,
+        @ApiParam @QueryParam("offset") @DefaultValue("0") Long offset,
+        @ApiParam @QueryParam("max") @DefaultValue(Constants.MAX_RESULTS_STRING) Integer max
+    ) {
+        StatsService.Period period;
+        try {
+            period = StatsService.Period.valueOf(periodString);
+        } catch(IllegalArgumentException e) {
+            return Responses.clientError("Can't parse period '{}'");
+        }
+
+        MediaResult result = playService.mediaScorePerBroadcaster(broadcaster, period, offset, max);
+        return filteredMediaResult(result, properties);
+    }
+
+    @GET
+    @Path("/media/portals/{portal}")
+    @ApiOperation(httpMethod = "get",
+        value = "Top media per portal",
+        notes = "High score list containing the most played media per portal")
+    @ApiErrors(value = {@ApiError(code = 400, reason = "Bad request"), @ApiError(code = 500, reason = "Server error")})
+    @Override
+    public Response statsPerPortal(
+        @ApiParam(required = true, defaultValue = "VproNL") @PathParam("portal") String portal,
+        @ApiParam(value = "On of hour/day/week") @QueryParam("period") @DefaultValue("hour") String periodString,
+        @ApiParam(value = "Optimise media result for these returned properties <a href=\"#!/media/load_get_0\">See Media API</a>", required = false) @QueryParam("properties") String properties,
+        @ApiParam @QueryParam("offset") @DefaultValue("0") Long offset,
+        @ApiParam @QueryParam("max") @DefaultValue(Constants.MAX_RESULTS_STRING) Integer max
+    ) {
+        StatsService.Period period;
+        try {
+            period = StatsService.Period.valueOf(periodString);
+        } catch(IllegalArgumentException e) {
+            return Responses.clientError("Can't parse period '{}'");
+        }
+
+        MediaResult result = playService.mediaScorePerPortal(portal, period, offset, max);
+        return filteredMediaResult(result, properties);
+    }
+
+
+    private Response filteredMediaResult(MediaResult result, String properties) {
+        if(properties == null) {
+            return Response.ok(result).build();
+        }
+
+        return Response.ok(MediaTransferResult.create(result, new PropertySelection(properties))).build();
+    }
+}
