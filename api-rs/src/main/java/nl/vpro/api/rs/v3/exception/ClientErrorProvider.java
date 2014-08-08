f1717eecd5c27622ccf2c2dadfa4c851ea3d09da
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2014-08-08 08:30:03 +0000

    Let's use actual status code.
    svn path=/nl/vpro/api/trunk/; revision=69393

--- api-rs/src/main/java/nl/vpro/api/rs/v3/exception/ClientErrorProvider.java
+++ api-rs/src/main/java/nl/vpro/api/rs/v3/exception/ClientErrorProvider.java
@@ -5,7 +5,6 @@
 package nl.vpro.api.rs.v3.exception;
 
 import javax.ws.rs.ClientErrorException;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -19,9 +18,9 @@ public class ClientErrorProvider implements ExceptionMapper<ClientErrorException
 
     @Override
     public Response toResponse(ClientErrorException exception) {
-        int statusCode = exception instanceof NotFoundException ? Response.Status.NOT_FOUND.getStatusCode() : Response.Status.BAD_REQUEST.getStatusCode();
-
-        return Response.ok(new nl.vpro.domain.api.Error(statusCode, exception.getMessage())).status(Response.Status.BAD_REQUEST).build();
+        int statusCode = exception.getResponse().getStatus();
+        return Response.ok(
+            new nl.vpro.domain.api.Error(statusCode, exception.getMessage())).status(statusCode).build();
     }
 
 }
