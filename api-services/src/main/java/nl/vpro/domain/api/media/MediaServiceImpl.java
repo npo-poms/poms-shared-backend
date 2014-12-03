23b0095a420b46870b6a20dfbc2b9d17d21290db
Roelof Jan Koekoek <r.koekoek@vpro.nl>
2014-12-03 09:34:35 +0000

    NPA-158 clean-up
    svn path=/nl/vpro/api/trunk/; revision=72846

---
+++ api-services/src/main/java/nl/vpro/domain/api/media/MediaServiceImpl.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.api.media;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import nl.vpro.domain.api.*;
+import nl.vpro.domain.api.profile.ProfileDefinition;
+import nl.vpro.domain.api.profile.ProfileService;
+import nl.vpro.domain.api.profile.exception.ProfileNotFoundException;
+import nl.vpro.domain.media.MediaObject;
+import nl.vpro.domain.media.MediaType;
+import nl.vpro.util.MaxOffsetIterator;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 2.0
+ */
+@Service
+public class MediaServiceImpl implements MediaService {
+
+    private final ProfileService profileService;
+
+    private final MediaRepository mediaRepository;
+
+    private final MediaSearchRepository mediaSearchRepository;
+
+    @Autowired
+    public MediaServiceImpl(
+        ProfileService profileService,
+        MediaRepository mediaRepository,
+        MediaSearchRepository mediaSearchRepository) {
+        this.profileService = profileService;
+        this.mediaRepository = mediaRepository;
+        this.mediaSearchRepository = mediaSearchRepository;
+    }
+
+    @Override
+    public Iterator<Change> changes(final String profile, final Long since, final Order order, final Integer max, final Long keepAlive) throws ProfileNotFoundException {
+        ProfileDefinition<MediaObject> currentProfile = profileService.getMediaProfileDefinition(profile); //getCombinedProfile(profile, since);
+
+        ProfileDefinition<MediaObject> previousProfile = since == null ? null : profileService.getMediaProfileDefinition(profile, since); //getCombinedProfile(profile, since);
+        if (currentProfile == null && previousProfile == null && profile != null) {
+            throw new ProfileNotFoundException("No such media profile " + profile);
+        }
+        return mediaRepository.changes(since, currentProfile, previousProfile, order, max, keepAlive);
+    }
+
+    @Override
+    public MediaObject load(String id) {
+        return mediaRepository.load(id);
+    }
+
+    @Override
+    public List<MediaObject> loadAll(List<String> ids) {
+        return mediaRepository.loadAll(ids);
+    }
+
+    @Override
+    public MediaResult list(Order order, Long offset, Integer max) {
+        return mediaRepository.list(order, offset, max);
+    }
+
+    @Override
+    public Iterator<MediaObject> iterate(String profile, MediaForm form, Long offset, Integer max) throws ProfileNotFoundException {
+        return new MaxOffsetIterator<>(mediaRepository.iterate(getProfile(profile), form), max, offset);
+    }
+
+    @Override
+    public MediaSearchResult find(String profile, MediaForm form, Long offset, Integer max) {
+        return mediaSearchRepository.find(getProfile(profile), form, offset, max);
+    }
+
+    @Override
+    public MediaResult listMembers(MediaObject media, Order order, Long offset, Integer max) {
+        return mediaRepository.listMembers(media, order, offset, max);
+    }
+
+    @Override
+    public MediaSearchResult findMembers(MediaObject media, String profile, MediaForm form, Long offset, Integer max) {
+        return mediaSearchRepository.findMembers(media, getProfile(profile), form, offset, max);
+    }
+
+    @Override
+    public ProgramResult listEpisodes(MediaObject media, Order order, Long offset, Integer max) {
+        return mediaRepository.listEpisodes(media, order, offset, max);
+    }
+
+    @Override
+    public ProgramSearchResult findEpisodes(MediaObject media, String profile, MediaForm form, Long offset, Integer max) {
+        return mediaSearchRepository.findEpisodes(media, getProfile(profile), form, offset, max);
+    }
+
+    @Override
+    public MediaResult listDescendants(MediaObject media, Order order, Long offset, Integer max) {
+        return mediaRepository.listDescendants(media, order, offset, max);
+    }
+
+    @Override
+    public MediaSearchResult findDescendants(MediaObject media, String profile, MediaForm form, Long offset, Integer max) {
+        return mediaSearchRepository.findDescendants(media, getProfile(profile), form, offset, max);
+    }
+
+    @Override
+    public MediaSearchResult findRelated(MediaObject media, String profile, MediaForm form, Integer max) {
+        return mediaSearchRepository.findRelated(media, getProfile(profile), form, max);
+    }
+
+    @Override
+    public MediaType getType(final String id) {
+        MediaObject owner = load(id);
+        return owner != null ? MediaType.getMediaType(owner) : null;
+    }
+
+    protected ProfileDefinition<MediaObject> getProfile(String profile) throws ProfileNotFoundException {
+        ProfileDefinition<MediaObject> def = null;
+        if (profile != null) {
+            def = profileService.getMediaProfileDefinition(profile);
+            if (def == null) {
+                throw new ProfileNotFoundException(profile);
+            }
+        }
+        return def;
+    }
+
+
+}
---
+++ api-services/src/test/java/nl/vpro/domain/api/media/MediaServiceImplTest.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.api.media;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import nl.vpro.domain.api.profile.ProfileDefinition;
+import nl.vpro.domain.api.profile.ProfileService;
+import nl.vpro.domain.api.profile.exception.ProfileNotFoundException;
+import nl.vpro.domain.constraint.media.Filter;
+import nl.vpro.domain.media.MediaObject;
+import nl.vpro.domain.media.MediaTestDataBuilder;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 2.0
+ */
+public class MediaServiceImplTest {
+
+    private final MediaRepository mediaRepository = Mockito.mock(MediaRepository.class);
+
+    private final MediaSearchRepository mediaSearchRepository = Mockito.mock(MediaSearchRepository.class);
+
+    private final ProfileService profileService = Mockito.mock(ProfileService.class);
+
+    private final MediaServiceImpl target = new MediaServiceImpl(profileService, mediaRepository, mediaSearchRepository);
+
+
+    private ProfileDefinition<MediaObject> profileDefinition = new ProfileDefinition<>();
+
+    private final MediaObject media = MediaTestDataBuilder.program().mid("POMS_12345").build();
+
+    private final MediaForm form = new MediaForm();
+
+    private final Long offset = 45l;
+
+    private final Integer max = 25;
+
+    @Before
+    public void setUp() {
+        Mockito.reset(profileService, mediaSearchRepository);
+        Filter filter = new Filter();
+        filter.setConstraint(MediaConstraints.and(
+            MediaConstraints.broadcaster("VPRO"),
+            MediaConstraints.hasImage()
+        ));
+        profileDefinition = new ProfileDefinition<>(filter);
+        when(profileService.getMediaProfileDefinition(eq("vpro"))).thenReturn(profileDefinition);
+    }
+
+    @Test
+    public void testLoad()  {
+        target.load(media.getMid());
+        verify(mediaRepository).load(media.getMid());
+    }
+
+    @Test
+    public void testFind() {
+        target.find("vpro", form, offset, max);
+        verify(mediaSearchRepository).find(profileDefinition, form, offset, max);
+    }
+
+    @Test
+    public void testFindMembers()  {
+        target.findMembers(media, "vpro", form, offset, max);
+        verify(mediaSearchRepository).findMembers(media, profileDefinition, form, offset, max);
+    }
+
+    @Test
+    public void testFindEpisodes() {
+        target.findEpisodes(media, "vpro", form, offset, max);
+        verify(mediaSearchRepository).findEpisodes(media, profileDefinition, form, offset, max);
+    }
+
+    @Test
+    public void testFindDescendants() {
+        target.findDescendants(media, "vpro", form, offset, max);
+        verify(mediaSearchRepository).findDescendants(media, profileDefinition, form, offset, max);
+    }
+
+    @Test
+    public void testFindRelated()  {
+        target.findRelated(media, "vpro", form, max);
+        verify(mediaSearchRepository).findRelated(media, profileDefinition, form, max);
+    }
+
+    @Test(expected = ProfileNotFoundException.class)
+    public void testChangesProfileNotFound() {
+        target.changes("notfound", 0l, null, 10, 100l);
+    }
+
+    @Test(expected = ProfileNotFoundException.class)
+    public void testIterateProfileNotFound() {
+        target.iterate("notfound", null, 0l, 10);
+    }
+
+    @Test(expected = ProfileNotFoundException.class)
+    public void testFindProfileNotFound() {
+        target.find("notfound", null, 0l, 10);
+    }
+
+    @Test(expected = ProfileNotFoundException.class)
+    public void testFindMembersProfileNotFound() {
+        target.findMembers(media, "notfound", null, 0l, 10);
+    }
+
+    @Test(expected = ProfileNotFoundException.class)
+    public void testFindEpisodesProfileNotFound() {
+        target.findEpisodes(media, "notfound", null, 0l, 10);
+    }
+
+    @Test(expected = ProfileNotFoundException.class)
+    public void testFindDescendantsProfileNotFound() {
+        target.findDescendants(media, "notfound", null, 0l, 10);
+    }
+
+    @Test(expected = ProfileNotFoundException.class)
+    public void testFindRelatedProfileNotFound() {
+        target.findRelated(media, "notfound", null, 10);
+    }
+    }
