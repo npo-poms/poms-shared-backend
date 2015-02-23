1a438774018bc33ef887efad5473b44e5b675dc0
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2015-02-23 09:33:10 +0000

    NPA-185
    svn path=/nl/vpro/api/trunk/; revision=75316

--- api-rs/src/main/java/nl/vpro/api/rs/v3/validation/AbstractValidatingReader.java
+++ api-rs/src/main/java/nl/vpro/api/rs/v3/validation/AbstractValidatingReader.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
@@ -23,6 +24,11 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.xml.sax.SAXException;
+
 /**
  * @author Michiel Meeuwissen
  * @since 3.4
@@ -30,13 +36,29 @@ import javax.xml.validation.SchemaFactory;
 @Consumes("application/xml")
 public abstract class AbstractValidatingReader<T> implements MessageBodyReader<T> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractValidatingReader.class);
+
 
     private final Unmarshaller unmarshaller;
     private final Class<T> classToRead;
 
+    @Value("${xml.input.validate}")
+    private boolean doValidate = true;
+
     public AbstractValidatingReader(Class<T> classToRead) {
         this.classToRead = classToRead;
         try {
+            unmarshaller = JAXBContext.newInstance(this.classToRead).createUnmarshaller();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostConstruct
+    public void init() throws JAXBException, IOException, SAXException {
+        if (doValidate) {
+            LOG.info("XML inputs for " + this.getClass().getName() + " will be validated (Setting xml.input.validate=true)");
             SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             JAXBContext context = JAXBContext.newInstance(this.classToRead);
             final DOMResult[] result = new DOMResult[1];
@@ -49,12 +71,13 @@ public abstract class AbstractValidatingReader<T> implements MessageBodyReader<T
                 }
             });
             Schema schema = sf.newSchema(new DOMSource(result[0].getNode()));
-            unmarshaller = JAXBContext.newInstance(this.classToRead).createUnmarshaller();
             unmarshaller.setSchema(schema);
-        } catch (Exception e) {
-
-            throw new RuntimeException(e);
+        } else {
+            LOG.info("XML inputs for " + this.getClass().getName() + " will not be validated (Setting xml.input.validate=false)");
         }
+    }
+    public void setDoValidate(boolean doValidate) {
+        this.doValidate = doValidate;
     }
 
     @Override
@@ -63,7 +86,7 @@ public abstract class AbstractValidatingReader<T> implements MessageBodyReader<T
     }
 
 
-    protected T unmarshal(InputStream inputStream) throws JAXBException {
+    public final T unmarshal(InputStream inputStream) throws JAXBException {
         return unmarshaller.unmarshal(new StreamSource(inputStream), classToRead).getValue();
     }
 
--- api-rs/src/test/java/nl/vpro/api/rs/v3/validation/PageFormValidatingReaderTest.java
+++ api-rs/src/test/java/nl/vpro/api/rs/v3/validation/PageFormValidatingReaderTest.java
@@ -1,18 +1,44 @@
 package nl.vpro.api.rs.v3.validation;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.UnmarshalException;
 
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 import nl.vpro.domain.api.page.PageForm;
 
 public class PageFormValidatingReaderTest {
 
+    private static final byte[] VALID_FORM = ("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+        "<pagesForm xmlns=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" highlight=\"false\">\n" +
+        "  <searches>\n" +
+        "    <sortDates>\n" +
+        "      <matcher>" +
+        "      <begin>2009-01-01T00:00:00+01:00</begin>\n" +
+        "      <end>2010-01-01T00:00:00+01:00</end>\n" +
+        "      </matcher>" +
+        "    </sortDates>\n" +
+        "  </searches>\n" +
+        "  <sortFields>\n" +
+        "    <sort order=\"ASC\">sortDate</sort>\n" +
+        "  </sortFields>\n" +
+        "  <facets>\n" +
+        "    <sortDates>\n" +
+        "      <interval>YEAR</interval>\n" +
+        "    </sortDates>\n" +
+        "  </facets>\n" +
+        "</pagesForm>\n").getBytes();
+
     @Test(expected = UnmarshalException.class)
     public void testReadFromInvalid() throws Exception {
         PageFormValidatingReader reader = new PageFormValidatingReader();
+        reader.setDoValidate(true);
+        reader.init();
 
         byte[] bytes = ("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
             "<pagesForm xmlns=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" highlight=\"false\">\n" +
@@ -37,26 +63,34 @@ public class PageFormValidatingReaderTest {
     @Test
     public void testRead() throws Exception {
         PageFormValidatingReader reader = new PageFormValidatingReader();
+        reader.setDoValidate(true);
+        reader.init();
+        PageForm form = reader.unmarshal(new ByteArrayInputStream(VALID_FORM));
+    }
 
-        byte[] bytes = ("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-            "<pagesForm xmlns=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" highlight=\"false\">\n" +
-            "  <searches>\n" +
-            "    <sortDates>\n" +
-            "      <matcher>" +
-            "      <begin>2009-01-01T00:00:00+01:00</begin>\n" +
-            "      <end>2010-01-01T00:00:00+01:00</end>\n" +
-            "      </matcher>" +
-            "    </sortDates>\n" +
-            "  </searches>\n" +
-            "  <sortFields>\n" +
-            "    <sort order=\"ASC\">sortDate</sort>\n" +
-            "  </sortFields>\n" +
-            "  <facets>\n" +
-            "    <sortDates>\n" +
-            "      <interval>YEAR</interval>\n" +
-            "    </sortDates>\n" +
-            "  </facets>\n" +
-            "</pagesForm>\n").getBytes();
-        PageForm form = reader.unmarshal(new ByteArrayInputStream(bytes));
+    @Test
+    public void testPerformance() throws JAXBException, IOException, SAXException {
+        long COUNT = 10000;
+        {
+            PageFormValidatingReader reader = new PageFormValidatingReader();
+            reader.setDoValidate(false);
+            reader.init();
+            long start = System.nanoTime();
+            for (int i = 0; i < COUNT; i++) {
+                reader.unmarshal(new ByteArrayInputStream(VALID_FORM));
+            }
+            System.out.println("Time to unmarshal (not validating) " + (1f * TimeUnit.MILLISECONDS.convert((System.nanoTime() - start), TimeUnit.NANOSECONDS) / COUNT) + " ms/unmarshal");
+        }
+        {
+            PageFormValidatingReader reader = new PageFormValidatingReader();
+            reader.setDoValidate(true);
+            reader.init();
+            long start = System.nanoTime();
+            for (int i = 0; i < COUNT; i++) {
+                reader.unmarshal(new ByteArrayInputStream(VALID_FORM));
+            }
+            System.out.println("Time to unmarshal (validating) " + (1f * TimeUnit.MILLISECONDS.convert((System.nanoTime() - start), TimeUnit.NANOSECONDS) / COUNT) + " ms/unmarshal");
+        }
+
     }
 }
