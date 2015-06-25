4f1d19c5a5eb59055c7f2bfa9f91e671cc8ec486
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2015-06-25 15:05:29 +0000

    Split of schedule-filter.
    svn path=/nl/vpro/api/trunk/; revision=79388

---
+++ api-rs-schedule-filter/src/main/java/nl/vpro/api/rs/v3/filter/ScheduleEventViewPredicate.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2014 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.api.rs.v3.filter;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.google.common.base.Predicate;
+
+import nl.vpro.domain.media.ScheduleEvent;
+
+/**
+ * @author rico
+ * @since 3.0
+ */
+public class ScheduleEventViewPredicate implements Predicate<ScheduleEvent> {
+    private static final Logger log = LoggerFactory.getLogger(ScheduleEventViewPredicate.class);
+
+    Date stop;
+
+    public ScheduleEventViewPredicate() {
+        Collection<? extends GrantedAuthority> roles = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        int daysToAdd = 3;
+        for(GrantedAuthority role : roles) {
+            String roleName = role.getAuthority();
+            if(roleName != null) {
+                switch(roleName) {
+                    case "ROLE_API_USER":
+                    case "ROLE_API_SUPERUSER":
+                    case "ROLE_API_SUPERCLIENT":
+                    case "ROLE_API_CHANGES_SUPERCLIENT":
+                        daysToAdd = -1;
+                        break;
+                    case "ROLE_API_CLIENT":
+                    case "ROLE_API_CHANGES_CLIENT":
+                        daysToAdd = 3;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        if(daysToAdd > 0) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DATE, daysToAdd);
+            stop = guideDayStart(calendar.getTime());
+            log.debug("Setting stop to " + stop);
+        }
+    }
+
+    @Override
+    public boolean apply(ScheduleEvent input) {
+        return (stop == null || input.getStart().compareTo(stop) <= 0);
+    }
+
+    private static Date guideDayStart(Date guideDay) {
+        DateTimeZone timeZone = DateTimeZone.forID("Europe/Amsterdam");
+        DateTime dateTime = new DateTime(guideDay, timeZone);
+        return dateTime.withHourOfDay(6).withMinuteOfHour(0).withSecondOfMinute(0).withMillisOfSecond(0).plusDays(1).toDate();
+    }
+
+}
