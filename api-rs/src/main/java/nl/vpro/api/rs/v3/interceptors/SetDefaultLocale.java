ad50ae6cc31c49b6755b583c58bcdeb16fb5f31d
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2016-11-08 20:34:23 +0000

    Acceptable languages.
    svn path=/nl/vpro/api/trunk/; revision=96420

---
+++ api-rs/src/main/java/nl/vpro/api/rs/v3/interceptors/SetDefaultLocale.java
@@ -0,0 +1,32 @@
+package nl.vpro.api.rs.v3.interceptors;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.ext.Provider;
+
+import nl.vpro.i18n.Locales;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 4.9
+ */
+@Provider
+@PreMatching
+public class SetDefaultLocale implements ContainerRequestFilter  {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        List<Locale> acceptableLanguages = requestContext.getAcceptableLanguages();
+        if (acceptableLanguages != null && ! acceptableLanguages.isEmpty()) {
+            Locales.setDefault(acceptableLanguages.get(0));
+        } else {
+            Locales.resetDefault();
+        }
+    }
+
+}
