ac056efecf2503822edc26350bf372010741d8cf
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2016-11-30 11:31:22 +0000

    Started testing FilteredList.
    svn path=/nl/vpro/api/trunk/; revision=97493

---
+++ api-rs-media-filter/src/test/java/nl/vpro/api/rs/v3/filter/FilteredListTest.java
@@ -0,0 +1,34 @@
+package nl.vpro.api.rs.v3.filter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.8
+ */
+public class FilteredListTest {
+    @Test
+    public void contains() throws Exception {
+        ApiMediaFilter.set("test:1");
+        List<String> list = Arrays.asList("a", "b");
+        FilteredList<String> filtered = new FilteredList<>("test", list);
+        assertThat(filtered.contains("b")).isFalse();
+
+    }
+
+
+    @Test
+    public void containsEmpty() throws Exception {
+        ApiMediaFilter.set("test:1");
+        List<String> list = Arrays.asList();
+        FilteredList<String> filtered = new FilteredList<>("test", list);
+        assertThat(filtered.contains("b")).isFalse();
+
+    }
+}
