13f8b631d2b6e14bd2bd0bba8d50fc88870a3404
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2016-12-12 13:50:25 +0000

    NPA-362
    svn path=/nl/vpro/api/trunk/; revision=98107

---
+++ api-rs-media-filter/src/main/java/nl/vpro/api/rs/v3/filter/FilteredSortedTitleSet.java
@@ -0,0 +1,39 @@
+package nl.vpro.api.rs.v3.filter;
+
+import java.util.Set;
+import java.util.SortedSet;
+
+import nl.vpro.domain.media.support.TextualType;
+import nl.vpro.domain.media.support.Title;
+import nl.vpro.domain.media.support.Typable;
+import nl.vpro.util.ResortedSortedSet;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.0
+ */
+public class FilteredSortedTitleSet extends FilteredSortedTextualTypableSet<Title> {
+
+
+    private FilteredSortedTitleSet(String property, SortedSet<Title> wrapped) {
+        super(property, wrapped);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Typable<TextualType>> FilteredSortedTitleSet wrapTitles(String property, Set<T> wrapped) {
+        if (!(wrapped instanceof SortedSet)) {
+            wrapped = new ResortedSortedSet<>(wrapped);
+        }
+
+        if (wrapped instanceof FilteredSortedTitleSet) {
+            if (!(((FilteredSortedTitleSet) wrapped).filterHelper.property).equals(property)) {
+                throw new IllegalArgumentException("Can't wrap different properties");
+            }
+
+            return (FilteredSortedTitleSet) wrapped;
+        }
+
+        return new FilteredSortedTitleSet(property, (SortedSet<Title>) wrapped);
+    }
+
+}
