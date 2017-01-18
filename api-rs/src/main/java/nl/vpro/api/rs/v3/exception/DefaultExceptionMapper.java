585e59479d171fe2be9ecee11889f29b375649b1
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2017-01-18 15:59:15 +0000

    NPA-346 If client decides to not handle the response, that need not cause stack traces in the server log.
    svn path=/nl/vpro/api/trunk/; revision=99365

--- api-rs/src/main/java/nl/vpro/api/rs/v3/exception/DefaultExceptionMapper.java
+++ api-rs/src/main/java/nl/vpro/api/rs/v3/exception/DefaultExceptionMapper.java
@@ -1,12 +1,12 @@
 package nl.vpro.api.rs.v3.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import org.jboss.resteasy.spi.Failure;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import nl.vpro.api.rs.v3.interceptors.StoreRequestInThreadLocal;
 
@@ -17,9 +17,8 @@ import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
  * @since 3.3
  */
 @Provider
+@Slf4j
 public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultExceptionMapper.class);
 
     @Override
     public Response toResponse(Exception exception) {
@@ -27,7 +26,7 @@ public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
             // E.g. a DefaultOptionsMethodException. If you catch those, CORS wont' work any more.
             return null;
         } else {
-            LOG.warn("Wrapped an {} root cause {}. Request: {}", exception.getClass().getName(), exception.getMessage(), StoreRequestInThreadLocal.getRequestBody());
+            log.warn("Wrapped an {} root cause {}. Request: {}", exception.getClass().getName(), exception.getMessage(), StoreRequestInThreadLocal.getRequestBody());
             return Response
                     .serverError()
                     .entity(new nl.vpro.domain.api.Error(INTERNAL_SERVER_ERROR, exception))
