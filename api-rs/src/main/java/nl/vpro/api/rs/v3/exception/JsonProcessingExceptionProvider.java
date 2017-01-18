677b0e58780ac89e868d0ee7e084739dedb0c11d
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2017-01-18 15:32:19 +0000

    Catch more client errors.
    svn path=/nl/vpro/api/trunk/; revision=99359

---
+++ api-rs/src/main/java/nl/vpro/api/rs/v3/exception/JsonProcessingExceptionProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2014 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.api.rs.v3.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import nl.vpro.api.rs.v3.interceptors.RSReaderInterceptor;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.1
+ */
+@Provider
+@Slf4j
+public class JsonProcessingExceptionProvider implements ExceptionMapper<JsonProcessingException> {
+
+    @Override
+    public Response toResponse(JsonProcessingException exception) {
+        if (RSReaderInterceptor.READEXCEPTION.get()) {
+            return Response
+                .status(BAD_REQUEST)
+                .entity(new nl.vpro.domain.api.Error(BAD_REQUEST, exception.getMessage()))
+                .build();
+        } else {
+            // error didn't happend during parsing of client's content. Perhaps some bug on the server (parsing ES or so?)
+            return Response
+                .status(INTERNAL_SERVER_ERROR)
+                .entity(new nl.vpro.domain.api.Error(INTERNAL_SERVER_ERROR, exception))
+                .build();
+        }
+    }
+
+}
---
+++ api-rs/src/main/java/nl/vpro/api/rs/v3/interceptors/RSReaderInterceptor.java
@@ -0,0 +1,36 @@
+package nl.vpro.api.rs.v3.interceptors;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.ReaderInterceptor;
+import javax.ws.rs.ext.ReaderInterceptorContext;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 4.1
+ */
+@Provider
+@Slf4j
+public class RSReaderInterceptor implements ReaderInterceptor {
+
+    public static ThreadLocal<Boolean> READEXCEPTION = ThreadLocal.withInitial(() -> false);
+
+    @Override
+    public Object aroundReadFrom(ReaderInterceptorContext context) throws IOException, WebApplicationException {
+        try {
+            READEXCEPTION.set(false);
+            log.debug("Start {}", context);
+            return context.proceed();
+        } catch (IOException | WebApplicationException readException) {
+            READEXCEPTION.set(true);
+            throw readException;
+        } finally {
+            log.debug("Stop {}", context);
+        }
+
+    }
+}
