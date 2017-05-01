e1869ca54bf02299b6666c7e932f9a8b71214148
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2017-05-01 12:34:00 +0000

    NPA-384
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=103000

--- api-backend-elasticsearch/src/test/java/nl/vpro/domain/api/suggest/ESQueryRepositoryTest.java
+++ api-backend-elasticsearch/src/test/java/nl/vpro/domain/api/suggest/ESQueryRepositoryTest.java
@@ -2,8 +2,8 @@ package nl.vpro.domain.api.suggest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.common.text.StringText;
 import org.elasticsearch.search.suggest.Suggest;
 import org.junit.Test;
@@ -42,10 +42,18 @@ public class ESQueryRepositoryTest {
         suggestion.addOption(new Suggest.Suggestion.Entry.Option(new StringText("lubach"), 0.5f));
         org.elasticsearch.search.suggest.Suggest suggest = new org.elasticsearch.search.suggest.Suggest(suggestionList);
 
-        SuggestResult adapt = instance.adapt(suggest, "liba", null);
+        String input = "luba";
+        SuggestResult adapt = instance.adapt(suggest, input, null);
         assertThat(adapt.getItems()).hasSize(5);
-        assertThat(adapt.getItems().stream().map(Suggestion::getText).collect(Collectors.toList()))
-            .containsExactly("cuba", "dubai", "libanon", "lubach", "libanon andere tijden");
+        int prevDistance = Integer.MIN_VALUE;
+        for (Suggestion sug : adapt.getItems()) {
+            int distance = StringUtils.getLevenshteinDistance(input, sug.getText());
+            System.out.println(distance + ":" + sug.getText());
+            assertThat(distance).isGreaterThanOrEqualTo(prevDistance);
+            prevDistance = distance;
+        }
+
+
     }
 
 }
