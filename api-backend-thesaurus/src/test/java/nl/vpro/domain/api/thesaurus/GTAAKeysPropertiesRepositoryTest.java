f3680a79d59a16be51e39ef4def1e6b2e3b4a106
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2017-08-31 11:23:56 +0000

    cleaned unused imports.
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=106596

--- api-backend-thesaurus/src/test/java/nl/vpro/domain/api/thesaurus/GTAAKeysPropertiesRepositoryTest.java
+++ api-backend-thesaurus/src/test/java/nl/vpro/domain/api/thesaurus/GTAAKeysPropertiesRepositoryTest.java
@@ -2,12 +2,8 @@ package nl.vpro.domain.api.thesaurus;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import nl.vpro.beeldengeluid.gtaa.GTAARepository;
-import nl.vpro.domain.api.thesaurus.GTAAKeysPropertiesRepository;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,7 +12,7 @@ public class GTAAKeysPropertiesRepositoryTest {
 
     @Autowired
     GTAAKeysPropertiesRepository repo;
-    
+
     @Test
     public void testGetKeyFor() {
         assertEquals("***REMOVED***", repo.getKeyFor("issuer1").get());
