7e063f8e8e320a38ecbd29485236de96eeee292a
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2017-09-13 09:49:47 +0000

    NPA-334
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=106916

--- api-backend-elasticsearch/src/test/java/nl/vpro/domain/api/ESClientTest.java
+++ api-backend-elasticsearch/src/test/java/nl/vpro/domain/api/ESClientTest.java
@@ -5,22 +5,24 @@ import lombok.extern.slf4j.Slf4j;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.node.Node;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * @author Michiel Meeuwissen
- * @since ...
+ * @since 5.5
  */
 @Slf4j
+@Ignore
 public class ESClientTest  {
 
 
@@ -40,8 +42,14 @@ public class ESClientTest  {
     @Test
     public void nodeTest() throws UnknownHostException {
         Settings settings = Settings.builder()
+            .put("path.home", "/tmp")
+            .put("node.master", false)
+            .put("node.data", false)
             .build();
-        NodeClient client = new NodeClient(settings, new ThreadPool(settings));
+        Node node = new Node(settings);
+
+        Client client = node.client();
+
         log.info("Built {}", client);
         QueryBuilder builder = QueryBuilders.matchAllQuery();
         log.info("Total hits {}", client.prepareSearch("apimedia").setSource(new SearchSourceBuilder().size(0).query(builder)).get().getHits().getTotalHits());
