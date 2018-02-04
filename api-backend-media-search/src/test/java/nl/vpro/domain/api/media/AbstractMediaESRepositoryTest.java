7de025a79c3d43820f6b63d7fc8e3505bf1ac903
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2018-02-04 10:33:08 +0000

    testing
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=110392

--- api-backend-media-search/src/test/java/nl/vpro/domain/api/media/AbstractMediaESRepositoryTest.java
+++ api-backend-media-search/src/test/java/nl/vpro/domain/api/media/AbstractMediaESRepositoryTest.java
@@ -2,11 +2,13 @@ package nl.vpro.domain.api.media;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.action.search.SearchRequest;
 import org.junit.Test;
 
+import nl.vpro.elasticsearch.ESClientFactory;
 import nl.vpro.media.domain.es.MediaESType;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Michiel Meeuwissen
@@ -15,20 +17,24 @@ import nl.vpro.media.domain.es.MediaESType;
 @Slf4j
 public class AbstractMediaESRepositoryTest {
 
-    AbstractESMediaRepository repository = new AbstractESMediaRepository(null) {
+    AbstractESMediaRepository repository = new AbstractESMediaRepository(mock(ESClientFactory.class)) {
         @Override
         protected String[] getRelevantTypes() {
             return MediaESType.mediaObjects();
         }
     };
+    {
+        repository.setIndexName("media");
+    }
 
     @Test
     public void searchRequest() {
-        BoolQueryBuilder filter = QueryBuilders.boolQuery();
-        repository.searchRequest(null, null,
-            MediaForm.builder().withEverything().build(), null,
-            filter, 0, 0);
-        log.info(filter.toString());
+
+        SearchRequest sr = repository.searchRequest(
+            null,
+            MediaForm.builder().withEverything().build(),
+            0, 0);
+        log.info(sr.toString());
 
     }
 }
