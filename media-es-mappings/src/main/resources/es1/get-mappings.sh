833864ac67b2bcba78cf45809f900126bb8835b1
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2018-12-07 16:00:34 +0000

    toot to get mappings
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=116593

---
+++ media-es-mappings/src/main/resources/es1/get-mappings.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ "$DEBUG" = "true" ] ; then
+    set -x
+fi
+
+if [ $# -lt 1 ];
+then
+   desthost=http://localhost:9200
+else
+   desthost=$1
+fi
+
+basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )"
+destindex=apimedia
+
+
+
+
+declare -a arr=( "group" "program" "segment" "deletedprogram" "deletedgroup" "deletedsegment" "cue" "programMemberRef" "groupMemberRef" "segmentMemberRef" "episodeRef" )
+
+mkdir gotmapping
+for i in "${!arr[@]}"
+do
+   curl $desthost/$destindex/_mapping/${arr[i]} | jq ".[].mappings.${arr[i]}" > $basedir/gotmapping/${arr[i]}.json
+ done
+
+
+
