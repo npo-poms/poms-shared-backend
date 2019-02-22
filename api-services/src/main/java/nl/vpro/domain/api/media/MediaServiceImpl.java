74ebd7ec87fae2fee49265b23f42bbcf757e459d
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2019-02-22 09:35:27 +0000

    NPA-471
    svn path=/nl/vpro/api/trunk/; revision=118184

--- api-services/src/main/java/nl/vpro/domain/api/media/MediaServiceImpl.java
+++ api-services/src/main/java/nl/vpro/domain/api/media/MediaServiceImpl.java
@@ -220,7 +220,7 @@ public class MediaServiceImpl implements MediaService {
     @Override
     @PreAuthorize(Roles.API_USER)
     public MediaSearchResult findRelatedInTopspin(MediaObject media, String profile, MediaForm form, Integer max) {
-        Recommendations recommendations = topSpinRepository.getForMid(media.getMid());
+        Recommendations recommendations = topSpinRepository.getForMid(media.getMid(), null);
         List<MediaObject> mediaObjects = loadAll(recommendations.getRecommendations().stream().map(Recommendation::getMidRef).collect(Collectors.toList()));
         Predicate<MediaObject> filter = (mo) -> true;
         if (profile != null) {
