9df08da0796e53e87618cf506c682ea5a1923acb
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2019-07-02 08:42:10 +0000

    NPA-486
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=120564

--- api-backend-media-search/src/main/java/nl/vpro/domain/api/schedule/ESScheduleRepository.java
+++ api-backend-media-search/src/main/java/nl/vpro/domain/api/schedule/ESScheduleRepository.java
@@ -204,6 +204,7 @@ public class ESScheduleRepository extends AbstractESMediaRepository implements S
                     query.must(QueryBuilders.termQuery("descendantOf.midRef", descendantOf));
                 }
             }
+
             if (form.hasStart() || form.hasStop()) {
                 RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("scheduleEvents.start");
                 if (form.hasStart()) {
@@ -233,12 +234,13 @@ public class ESScheduleRepository extends AbstractESMediaRepository implements S
         ElasticSearchIterator<MediaObject> searchIterator = new ElasticSearchIterator<>(client(), this::getMediaObject);
         SearchRequestBuilder requestBuilder = searchIterator.prepareSearch(getIndexName());
         requestBuilder.setQuery(toExecute);
-        requestBuilder.addSort("creationDate", SortOrder.DESC);
+        requestBuilder.addSort("sortDate", SortOrder.DESC);
         requestBuilder.setTypes(getScheduleEventTypes());
 
         List<ApiScheduleEvent> results = new ArrayList<>();
 
         long skipped = 0;
+
 
         long count = 0;
         long mediaObjectCount = 0;
