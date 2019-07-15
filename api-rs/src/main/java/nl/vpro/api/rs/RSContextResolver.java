a2dc8490e5fe2e0279cd05220f77138720d9be04
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2019-07-15 20:10:56 +0000

    Resteasy upgrade.
    svn path=/nl/vpro/api/trunk/; revision=120938

---
+++ api-rs/src/main/java/nl/vpro/api/rs/RSContextResolver.java
@@ -0,0 +1,40 @@
+package nl.vpro.api.rs;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+
+import org.jboss.resteasy.api.validation.ResteasyConstraintViolation;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.10
+ */
+@Provider
+public class RSContextResolver implements ContextResolver<JAXBContext> {
+
+
+    public static final JAXBContext ERROR_CONTEXT;
+    static {
+        try {
+            JAXBContext jxb = JAXBContext.newInstance(
+                nl.vpro.domain.api.Error.class,
+                ResteasyConstraintViolation.class
+
+            );
+            ERROR_CONTEXT = jxb;
+        } catch (JAXBException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public JAXBContext getContext(Class<?> type) {
+        if (type.equals(nl.vpro.domain.api.Error.class)) {
+            return ERROR_CONTEXT;
+        } else {
+            return null;
+        }
+    }
+}
--- api-rs/src/main/java/nl/vpro/api/rs/exception/ValidationExceptionProvider.java
+++ api-rs/src/main/java/nl/vpro/api/rs/exception/ValidationExceptionProvider.java
@@ -7,14 +7,10 @@ package nl.vpro.api.rs.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
-
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import javax.xml.bind.annotation.XmlElement;
 
-import org.jboss.resteasy.api.validation.ResteasyConstraintViolation;
 import org.jboss.resteasy.api.validation.ResteasyViolationException;
 
 import nl.vpro.api.rs.interceptors.StoreRequestInThreadLocal;
@@ -34,20 +30,11 @@ public class ValidationExceptionProvider implements ExceptionMapper<ResteasyViol
         final String violationsAsString = e.getViolations().toString().replace('\r', ' ');
 
         log.info("Bad request/Violation exception: {}. Request: {}", violationsAsString, StoreRequestInThreadLocal.getRequestBody());
-
+        nl.vpro.domain.api.Error error = new nl.vpro.domain.api.Error(BAD_REQUEST, violationsAsString);
+        error.setViolations(e.getViolations());
         return Response
                 .status(BAD_REQUEST)
-                .entity(new nl.vpro.domain.api.Error(BAD_REQUEST, violationsAsString) {
-            @XmlElement
-            public List<ResteasyConstraintViolation> getViolations() {
-                return e.getViolations();
-            }
-
-            @Override
-            public String toString() {
-                return (super.toString() + ":" + violationsAsString);
-            }
-        })
+                .entity(error)
                 .build();
     }
 
