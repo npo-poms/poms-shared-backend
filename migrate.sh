9799f0a576d6451614b96c8295839b673b286455
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2019-11-15 09:28:44 +0000

    parameterized it for localhost testing.
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=123355

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
-REMOTE=$1
+
 source="http://pomz11aas:9200"
 dest="localhost:9215"
+while getopts ":s:d:" opt; do
+  case ${opt} in
+    s )
+      source=$OPTARG
+      echo source: $source
+      ;;
+    d )
+      dest=$OPTARG
+      ;;
+    \? ) echo "Usage: $0 [-s <source>]  [-d <destination>]"
+      exit
+      ;;
+   esac
+done
 #set -x
 function reindex {
     command=$( jq -n \
@@ -20,7 +34,7 @@ function reindex {
   }
 }')
 
-echo $command
+echo $dest $command
 curl -X POST "$dest/_reindex?wait_for_completion=false" -H 'Content-Type: application/json' -d"$command"
 echo
   }
@@ -47,7 +61,7 @@ function reindexType {
   }
 }')
 
-echo $command
+echo $dest $command
 curl -X POST "$dest/_reindex?wait_for_completion=false" -H 'Content-Type: application/json' -d"$command"
 echo
  }
