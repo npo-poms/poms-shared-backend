24ede692c0994b066a8c83119cb4f317034f19ae
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2020-02-10 10:42:50 +0000

    Logging.
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=124682

--- push-es-mappings/src/main/java/nl/vpro/npopublisher/pushesmappings/PushMappings.java
+++ push-es-mappings/src/main/java/nl/vpro/npopublisher/pushesmappings/PushMappings.java
@@ -36,7 +36,7 @@ public class PushMappings {
         if (argv.length > 0) {
             factory.setUnicastHosts(argv[0]);
         } else {
-            factory.setUnicastHosts("http://localhost:9215");
+            factory.setUnicastHosts("http://localhost:9221");
         }
         if (argv.length > 1) {
             factory.setClusterName(argv[1]);
