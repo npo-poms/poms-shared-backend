b588e1380ade8e48999a9b9729fe71a9bf03e85c
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2020-06-18 14:40:29 +0000

    NPA-547
    svn path=/nl/publiekeomroep/poms-shared-backend/trunk/; revision=126792

---
+++ media-es-mappings/src/main/java/nl/vpro/media/domain/es/Common.java
@@ -0,0 +1,15 @@
+package nl.vpro.media.domain.es;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.13
+ */
+public class Common {
+
+    private Common() {
+
+    }
+
+    public static final String ES_PUBLISH_DATE = "esPublishDate";
+
+}
