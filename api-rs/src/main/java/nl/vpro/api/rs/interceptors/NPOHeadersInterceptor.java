5df1a081ceccb50c9bec70bbdc595b16455600cf
Michiel Meeuwissen <git@meeuw.org>
2021-10-19 16:54:40 +0200

    Smarter extra headers.

--- api-rs/src/main/java/nl/vpro/api/rs/interceptors/NPOHeadersInterceptor.java
+++ api-rs/src/main/java/nl/vpro/api/rs/interceptors/NPOHeadersInterceptor.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import nl.vpro.VersionService;
 import nl.vpro.domain.api.Result;
 import nl.vpro.domain.api.media.Redirector;
+import nl.vpro.poms.shared.ExtraHeaders;
 import nl.vpro.poms.shared.Headers;
 
 
@@ -24,19 +25,28 @@ import nl.vpro.poms.shared.Headers;
 public class NPOHeadersInterceptor implements ContainerResponseFilter, ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        Map<String, String> redirects = Redirector.REDIRECTS.get();
-        if (redirects != null && ! redirects.isEmpty()) {
-            response.getHeaders().putSingle(Headers.NPO_REDIRECTS, redirects);
+        try {
+            Map<String, String> redirects = Redirector.REDIRECTS.get();
+            if (redirects != null && !redirects.isEmpty()) {
+                response.getHeaders().putSingle(Headers.NPO_REDIRECTS, redirects);
+            }
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null) {
+                response.getHeaders().putSingle(Headers.NPO_CURRENT_USER, authentication.getName());
+            }
+            response.getHeaders().putSingle(Headers.NPO_VERSION, VersionService.version());
+
+            ExtraHeaders.get().forEach(p -> {
+                response.getHeaders().putSingle(p.getKey(), p.getValue());
+            });
+            Object entity = response.getEntity();
+            if (entity instanceof Result) {
+                response.getHeaders().putSingle(Headers.NPO_TOOK, ((Result<?>) entity).getTook());
+            }
+        } finally {
+            ExtraHeaders.remove();
         }
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null) {
-            response.getHeaders().putSingle(Headers.NPO_CURRENT_USER, authentication.getName());
-        }
-        Object entity = response.getEntity();
-        if (entity instanceof Result) {
-            response.getHeaders().putSingle(Headers.NPO_TOOK, ((Result) entity).getTook());
-        }
-        response.getHeaders().putSingle(Headers.NPO_VERSION, VersionService.version());
+
 
     }
 
