85060b069fbff728db327754f48576a9b4086d90
Michiel Meeuwissen <git@meeuw.org>
2021-11-12 14:54:05 +0100

    NPA-604. Beter script and possibility to override min score.

--- api-backend-elasticsearch/src/main/java/nl/vpro/domain/api/SemanticScoring.java
+++ api-backend-elasticsearch/src/main/java/nl/vpro/domain/api/SemanticScoring.java
@@ -1,13 +1,18 @@
 package nl.vpro.domain.api;
 
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.text.MessageFormat;
+import java.util.*;
 
+import org.apache.commons.lang3.StringUtils;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
 
 import com.google.common.annotations.Beta;
 
@@ -19,13 +24,21 @@ import nl.vpro.semantic.VectorizationService;
  *
  */
 @Beta
+@ManagedResource(objectName = "nl.vpro.api:name=SemanticScoring")
+@Component
 public class SemanticScoring {
 
-    private SemanticScoring() {
-        // utilily
+    public SemanticScoring() {
+
     }
 
-    public static <MT extends MatchType> Script buildSemanticScoreScript(
+    @Value("${semantic.score.script}")
+    private String script = "doc[''{0}''].size() == 0 ? 0.0  : (cosineSimilarity(params.query_vector, doc[''{0}'']) + 1) / 2";
+
+    @Value("${semantic.score.min:#{null}}")
+    private Float minScore;
+
+    public <MT extends MatchType> Script buildSemanticScoreScript(
         @NonNull String prefix,
         @NonNull AbstractTextMatcher<MT> textSearch,
         @Nullable VectorizationService vectorizationService) {
@@ -37,7 +50,29 @@ public class SemanticScoring {
         Map<String, Object> params = new LinkedHashMap<>();
         params.put("query_vector", vectorization);
         String field = prefix + Common.ES_VECTORIZATION;
-        Script script = new Script(ScriptType.INLINE, "painless", "doc['" + field + "'].size() == 0 ? 0.0  : (cosineSimilarity(params.query_vector, doc['"  + field + "']) + 2)", params);
+        return  new Script(ScriptType.INLINE, "painless", MessageFormat.format(script, field), params);
+    }
+
+    @ManagedAttribute
+    public String getScript() {
         return script;
     }
+    @ManagedAttribute
+    public void setScript(String script) {
+        this.script = script;
+    }
+
+    @ManagedAttribute
+    public String getMinScore() {
+        return minScore == null ? "" : "" + minScore;
+    }
+    @ManagedAttribute
+    public void setMinScore(String score) {
+        this.minScore = StringUtils.isEmpty(score) ? null : Float.parseFloat(score);
+    }
+
+    public Optional<Float> getMinScoreOverride() {
+        return Optional.ofNullable(minScore);
+    }
+
 }
