0a9500f3c3e926c7b7e3f28ad3b0146ff0382b67
Michiel Meeuwissen <git@meeuw.org>
2022-08-19 10:32:54 +0200

    Migrated to log4j2.

--- api-backend-media-search/src/main/java/nl/vpro/domain/api/media/ESMediaRepository.java
+++ api-backend-media-search/src/main/java/nl/vpro/domain/api/media/ESMediaRepository.java
@@ -6,6 +6,7 @@ package nl.vpro.domain.api.media;
 
 import lombok.Getter;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -51,6 +52,7 @@ import static nl.vpro.domain.media.StandaloneMemberRef.ObjectType.memberRef;
  * @since 2.0
  */
 @Slf4j
+@Log4j2
 @ManagedResource(objectName = "nl.vpro.api:name=ESMediaRepository")
 public class ESMediaRepository extends AbstractESMediaRepository implements MediaSearchRepository {
 
