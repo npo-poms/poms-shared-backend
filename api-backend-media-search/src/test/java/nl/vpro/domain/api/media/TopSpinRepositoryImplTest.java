c5fbfe988fd5c4b6dd6efa5679f968fcd360e9da
Michiel Meeuwissen <git@meeuw.org>
2022-11-10 11:05:00 +0100

    Dropped ru.lanwen.

--- api-backend-media-search/src/test/java/nl/vpro/domain/api/media/TopSpinRepositoryImplTest.java
+++ api-backend-media-search/src/test/java/nl/vpro/domain/api/media/TopSpinRepositoryImplTest.java
@@ -1,16 +1,14 @@
 package nl.vpro.domain.api.media;
 
-import ru.lanwen.wiremock.ext.WiremockResolver;
-import ru.lanwen.wiremock.ext.WiremockUriResolver;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
 import nl.vpro.domain.api.topspin.Recommendations;
 
@@ -18,32 +16,27 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static ru.lanwen.wiremock.ext.WiremockResolver.Wiremock;
-import static ru.lanwen.wiremock.ext.WiremockUriResolver.WiremockUri;
 
-@ExtendWith({
-    WiremockResolver.class,
-    WiremockUriResolver.class
-})
+@WireMockTest
 public class TopSpinRepositoryImplTest {
 
 
     @Test
-    public void testNoExists(@Wiremock WireMockServer server, @WiremockUri String url) {
-        server.stubFor(get(urlEqualTo("/notexist")).willReturn(notFound()));
+    public void testNoExists(WireMockRuntimeInfo wireMockRuntimeInfo) {
+        WireMock.stubFor(get(urlEqualTo("/notexist")).willReturn(notFound()));
         TopSpinRepositoryImpl repo = new TopSpinRepositoryImpl();
-        repo.topspinUrl = url + "/{mediaId}";
+        repo.topspinUrl = wireMockRuntimeInfo.getHttpBaseUrl() + "/{mediaId}";
         Recommendations forMid = repo.getForMid("notexist", null, null);
         assertNotNull(forMid);
         assertTrue(forMid.getRecommendations().isEmpty());
     }
 
     @Test
-    public void testExists(@Wiremock WireMockServer server, @WiremockUri String url) throws IOException {
+    public void testExists(WireMockRuntimeInfo wireMockRuntimeInfo) throws IOException {
         String json = IOUtils.toString(getClass().getResourceAsStream("/topspin-response.json"), StandardCharsets.UTF_8);
-        server.stubFor(get(urlEqualTo("/exists")).willReturn(okJson(json)));
+        WireMock.stubFor(get(urlEqualTo("/exists")).willReturn(okJson(json)));
         TopSpinRepositoryImpl repo = new TopSpinRepositoryImpl();
-        repo.topspinUrl = url + "/{mediaId}";
+        repo.topspinUrl = wireMockRuntimeInfo.getHttpBaseUrl() + "/{mediaId}";
         Recommendations forMid = repo.getForMid("exists", null, null);
         assertThat(forMid.getRecommendations().size()).isEqualTo(87);
     }
