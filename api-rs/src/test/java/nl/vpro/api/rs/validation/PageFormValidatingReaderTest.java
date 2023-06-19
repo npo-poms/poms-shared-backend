fed532f1a70731bbc6cee03ddd44ebcbd15d5169
Michiel Meeuwissen <git@meeuw.org>
2023-06-19 15:34:59 +0200

    Text blocks.

--- api-rs/src/test/java/nl/vpro/api/rs/validation/PageFormValidatingReaderTest.java
+++ api-rs/src/test/java/nl/vpro/api/rs/validation/PageFormValidatingReaderTest.java
@@ -16,25 +16,25 @@ import nl.vpro.domain.api.page.PageForm;
 
 public class PageFormValidatingReaderTest {
 
-    private static final byte[] VALID_FORM = ("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-        "<pagesForm xmlns=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" highlight=\"false\">\n" +
-        "  <searches>\n" +
-        "    <sortDates>\n" +
-        "      <matcher>" +
-        "      <begin>2009-01-01T00:00:00+01:00</begin>\n" +
-        "      <end>2010-01-01T00:00:00+01:00</end>\n" +
-        "      </matcher>" +
-        "    </sortDates>\n" +
-        "  </searches>\n" +
-        "  <sortFields>\n" +
-        "    <sort order=\"ASC\">sortDate</sort>\n" +
-        "  </sortFields>\n" +
-        "  <facets>\n" +
-        "    <sortDates>\n" +
-        "      <interval>YEAR</interval>\n" +
-        "    </sortDates>\n" +
-        "  </facets>\n" +
-        "</pagesForm>\n").getBytes();
+    private static final byte[] VALID_FORM = ("""
+        <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+        <pagesForm xmlns="urn:vpro:api:2013" xmlns:media="urn:vpro:media:2009" highlight="false">
+          <searches>
+            <sortDates>
+              <matcher>      <begin>2009-01-01T00:00:00+01:00</begin>
+              <end>2010-01-01T00:00:00+01:00</end>
+              </matcher>    </sortDates>
+          </searches>
+          <sortFields>
+            <sort order="ASC">sortDate</sort>
+          </sortFields>
+          <facets>
+            <sortDates>
+              <interval>YEAR</interval>
+            </sortDates>
+          </facets>
+        </pagesForm>
+        """).getBytes();
 
     @Test
     public void testReadFromInvalid() {
@@ -43,23 +43,25 @@ public class PageFormValidatingReaderTest {
             reader.setDoValidate(true);
             reader.init();
 
-        byte[] bytes = ("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-            "<pagesForm xmlns=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" highlight=\"false\">\n" +
-            "  <searches>\n" +
-            "    <sortDatee>\n" +
-            "      <begin>2009-01-01T00:00:00+01:00</begin>\n" +
-            "      <end>2010-01-01T00:00:00+01:00</end>\n" +
-            "    </sortDatee>\n" +
-            "  </searches>\n" +
-            "  <sortFields>\n" +
-            "    <sort order=\"ASC\">sortDate</sort>\n" +
-            "  </sortFields>\n" +
-            "  <facets>\n" +
-            "    <sortDates>\n" +
-            "      <interval>YEAR</interval>\n" +
-            "    </sortDates>\n" +
-            "  </facets>\n" +
-            "</pagesForm>\n").getBytes();
+        byte[] bytes = ("""
+            <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <pagesForm xmlns="urn:vpro:api:2013" xmlns:media="urn:vpro:media:2009" highlight="false">
+              <searches>
+                <sortDatee>
+                  <begin>2009-01-01T00:00:00+01:00</begin>
+                  <end>2010-01-01T00:00:00+01:00</end>
+                </sortDatee>
+              </searches>
+              <sortFields>
+                <sort order="ASC">sortDate</sort>
+              </sortFields>
+              <facets>
+                <sortDates>
+                  <interval>YEAR</interval>
+                </sortDates>
+              </facets>
+            </pagesForm>
+            """).getBytes();
         reader.unmarshal(new ByteArrayInputStream(bytes));
         }).isInstanceOf(UnmarshalException.class);
     }
