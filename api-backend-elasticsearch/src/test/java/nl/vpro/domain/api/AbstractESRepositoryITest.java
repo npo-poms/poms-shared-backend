440df5e982b12632c2a38307ec2b4b0080970313
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-11-15 09:14:02 +0100

    readded for backwards compatibility

---
+++ api-backend-elasticsearch/src/test/java/nl/vpro/domain/api/AbstractESRepositoryITest.java
@@ -0,0 +1,11 @@
+package nl.vpro.domain.api;
+
+/**
+ * @author Michiel Meeuwissen
+ * @deprecated  Use {@link AbstractESRepositoryContainerTest}
+ */
+@Deprecated
+public abstract class AbstractESRepositoryITest extends AbstractESRepositoryContainerTest {
+
+
+}
